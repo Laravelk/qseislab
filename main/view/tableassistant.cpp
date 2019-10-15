cf48bc1e34aeac2700c7c230a34c01ede0dfa430
mgalushkin <m.galushkin@g.nsu.ru>
2019-10-15 19:07:08 +0700

    15.10-nsu

--- main/view/tableassistant.cpp
+++ main/view/tableassistant.cpp
@@ -143,88 +143,90 @@ void TableAssistant::insertRowInFilterTable(const QString &field) {
   _filterTable->setCellWidget(row, 2, lineEdit);
 }
 
-template <typename param_t> param_t castToParam(const QString &value) {
-  if constexpr (std::is_same_v<QString, param_t>) {
-    return value;
-  } else if constexpr (std::is_same_v<std::string, param_t>) {
-    return value.toStdString();
-  } else if constexpr (std::is_same_v<QDate, param_t>) {
-    return QDate::fromString(value);
-  } else if constexpr (std::is_same_v<QTime, param_t>) {
-    return QTime::fromString(value);
-  } else if constexpr (std::is_same_v<int, param_t>) {
-    return std::stoi(value.toStdString());
-  } else if constexpr (std::is_same_v<float, param_t>) {
-    return std::stof(value.toStdString());
-  } else {
-    assert(false);
-  }
-}
+// template <typename param_t> param_t castToParam(const QString &value) {
+//  if constexpr (std::is_same_v<QString, param_t>) {
+//    return value;
+//  } else if constexpr (std::is_same_v<std::string, param_t>) {
+//    return value.toStdString();
+//  } else if constexpr (std::is_same_v<QDate, param_t>) {
+//    return QDate::fromString(value);
+//  } else if constexpr (std::is_same_v<QTime, param_t>) {
+//    return QTime::fromString(value);
+//  } else if constexpr (std::is_same_v<int, param_t>) {
+//    return std::stoi(value.toStdString());
+//  } else if constexpr (std::is_same_v<float, param_t>) {
+//    return std::stof(value.toStdString());
+//  } else {
+//    assert(false);
+//  }
+//}
 
-template <typename param_t>
-void TableAssistant::parse(int enable, const QString &pattern,
-                           const QString &filterName, int column) {
-  std::cout << "pattern == " << pattern.toStdString() << std::endl;
-  std::cout << "filterName == " << filterName.toStdString() << std::endl;
+// template <typename param_t>
+// void parse(TableAssistant *assist, int enable, const QString &pattern,
+//           const QString &filterName, int column) {
+//  std::cout << "pattern == " << pattern.toStdString() << std::endl;
+//  std::cout << "filterName == " << filterName.toStdString() << std::endl;
 
-  typedef std::string::const_iterator It;
-  It phrase_begin(pattern.toStdString().begin());
-  It phrase_end(pattern.toStdString().end());
+//  typedef std::string::const_iterator It;
+//  It phrase_begin(pattern.toStdString().begin());
+//  It phrase_end(pattern.toStdString().end());
 
-  TableFilterParsing::Parser<param_t, It> parser;
+//  TableFilterParsing::Parser<param_t, It> parser;
 
-  TableFilterParsing::expr<param_t> result;
-  bool ok = true;
-  try {
-    ok = qi::phrase_parse(phrase_begin, phrase_end, parser, qi::space, result);
-  } catch (...) {
-    // TODO: send Message (bad parse)
-    //    std::exception_ptr p = std::current_exception();
-    //    std::cerr << (p ? p.__cxa_exception_type()->name() : "null") <<
-    //    std::endl; handle_eptr(p);
-    std::cerr << "bad parse" << std::endl;
-    return;
-  }
-  std::cout << "ok == " << ok << std::endl;
+//  TableFilterParsing::expr<param_t> result;
+//  bool ok = true;
+//  try {
+//    ok = qi::phrase_parse(phrase_begin, phrase_end, parser, qi::space,
+//    result);
+//  } catch (...) {
+//    // TODO: send Message (bad parse)
+//    //    std::exception_ptr p = std::current_exception();
+//    //    std::cerr << (p ? p.__cxa_exception_type()->name() : "null") <<
+//    //    std::endl; handle_eptr(p);
+//    std::cerr << "bad parse" << std::endl;
+//    return;
+//  }
+//  std::cout << "ok == " << ok << std::endl;
 
-  if constexpr (std::is_same_v<int, param_t>) {
-    std::cout << "param_t = int" << std::endl;
-  }
+//  if constexpr (std::is_same_v<int, param_t>) {
+//    std::cout << "param_t = int" << std::endl;
+//  }
 
-  for (int row = 0; row < _objectsTable->rowCount(); ++row) {
-    auto item = _objectsTable->item(row, column);
-    if (item) {
-      QStringList filterNames = _objectsTable->item(row, 1)->text().split(';');
-      if (Qt::Checked == enable) {
-        // if (applicable(pattern, item->text())) {
-        bool eval;
-        try {
-          param_t param = castToParam<param_t>(item->text());
-          if constexpr (std::is_same_v<int, param_t>) {
-            std::cout << "param(int) == " << param << std::endl;
-          }
-          eval = parser.evaluate(result, param);
-        } catch (...) {
-          std::cerr << "Bar eval" << std::endl;
-          // TODO: send Message (table value is bad)
-          return;
-        }
-        if (eval) {
-          if (1 == filterNames.count()) {
-            _objectsTable->setRowHidden(row, true);
-          }
-          filterNames.append(filterName);
-        }
-      } else if (Qt::Unchecked == enable) {
-        filterNames.removeAll(filterName);
-        if (1 == filterNames.count()) {
-          _objectsTable->setRowHidden(row, false);
-        }
-      }
-      _objectsTable->item(row, 1)->setText(filterNames.join(';'));
-    }
-  }
-}
+//  for (int row = 0; row < assist->_objectsTable->rowCount(); ++row) {
+//    auto item = assist->_objectsTable->item(row, column);
+//    if (item) {
+//      QStringList filterNames =
+//          assist->_objectsTable->item(row, 1)->text().split(';');
+//      if (Qt::Checked == enable) {
+//        // if (applicable(pattern, item->text())) {
+//        bool eval;
+//        try {
+//          param_t param = castToParam<param_t>(item->text());
+//          if constexpr (std::is_same_v<int, param_t>) {
+//            std::cout << "param(int) == " << param << std::endl;
+//          }
+//          eval = parser.evaluate(result, param);
+//        } catch (...) {
+//          std::cerr << "Bar eval" << std::endl;
+//          // TODO: send Message (table value is bad)
+//          return;
+//        }
+//        if (eval) {
+//          if (1 == filterNames.count()) {
+//            assist->_objectsTable->setRowHidden(row, true);
+//          }
+//          filterNames.append(filterName);
+//        }
+//      } else if (Qt::Unchecked == enable) {
+//        filterNames.removeAll(filterName);
+//        if (1 == filterNames.count()) {
+//          assist->_objectsTable->setRowHidden(row, false);
+//        }
+//      }
+//      assist->_objectsTable->item(row, 1)->setText(filterNames.join(';'));
+//    }
+//  }
+//}
 
 void TableAssistant::enbledFilter(int enable, const QString &filterName,
                                   const QString &pattern) {
@@ -238,19 +240,27 @@ void TableAssistant::enbledFilter(int enable, const QString &filterName,
       switch (column) {
       case 3:
       case 4:
-        parse<int>(enable, pattern, filterName, column);
+        typedef std::string::const_iterator It;
+        It phrase_begin(pattern.toStdString().begin());
+        It phrase_end(pattern.toStdString().end());
+        TableFilterParsing::Parser<QString, It> parser;
+        TableFilterParsing::expr<QString> result;
+        bool ok = qi::phrase_parse(phrase_begin, phrase_end, parser, qi::space,
+                                   result);
+        std::cout << "ok == " << ok << std::endl;
+        //        parse<int>(this, enable, pattern, filterName, column);
         break;
-      case 5:
-      case 6:
-      case 7:
-        parse<float>(enable, pattern, filterName, column);
-        break;
-      case 8:
-        parse<QDate>(enable, pattern, filterName, column);
-        break;
-      case 9:
-        parse<QTime>(enable, pattern, filterName, column);
-        break;
+        //      case 5:
+        //      case 6:
+        //      case 7:
+        //        parse<QString>(this, enable, pattern, filterName, column);
+        //        break;
+        //      case 8:
+        //        parse<QString>(this, enable, pattern, filterName, column);
+        //        break;
+        //      case 9:
+        //        parse<QTime>(this, enable, pattern, filterName, column);
+        //        break;
       }
       return;
     }
