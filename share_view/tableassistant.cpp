a263af842c4f65e48486f5ccb0fe541e44d23599
mgalushkin <m.galushkin@g.nsu.ru>
2019-10-21 12:42:48 +0700

    add buttons to table

--- share_view/tableassistant.cpp
+++ share_view/tableassistant.cpp
@@ -39,13 +39,6 @@ TableAssistant::TableAssistant(Mode mode, QWidget *parent)
                 _objectsTable->item(row, 0)->data(Qt::DisplayRole).toUuid());
           });
 
-  auto horizontalHeaderObjectTable = _objectsTable->horizontalHeader();
-  horizontalHeaderObjectTable->setSectionResizeMode(
-      2, QHeaderView::Fixed); // fixed remove-button section
-  horizontalHeaderObjectTable->setStretchLastSection(true);
-  horizontalHeaderObjectTable->setDefaultAlignment(Qt::AlignLeft);
-  _objectsTable->verticalHeader()->setVisible(false);
-
   _filterTable->setEditTriggers(QAbstractItemView::NoEditTriggers);
   _filterTable->setSelectionBehavior(QAbstractItemView::SelectRows);
   _filterTable->setSelectionMode(QAbstractItemView::SingleSelection);
@@ -64,9 +57,15 @@ TableAssistant::TableAssistant(Mode mode, QWidget *parent)
     insertRowInFilterTable(_objectsTable->horizontalHeaderItem(i)->text());
   }
   _filterTable->resizeColumnsToContents();
+  _filterTable->setHidden(true);
+
+  QPushButton *hideButton = new QPushButton("filters");
   // Settings end
 
   // Conections
+  connect(hideButton, &QPushButton::clicked, [this]() {
+    this->_filterTable->setVisible(!this->_filterTable->isVisible());
+  });
   // Conections end
 
   // Layouts
@@ -74,7 +73,11 @@ TableAssistant::TableAssistant(Mode mode, QWidget *parent)
   splitter->setChildrenCollapsible(false);
   splitter->addWidget(_filterTable);
   splitter->addWidget(_objectsTable);
+  QHBoxLayout *buttonsLayout = new QHBoxLayout();
+  buttonsLayout->addWidget(hideButton);
+  buttonsLayout->addStretch(1);
   QVBoxLayout *mainLayout = new QVBoxLayout();
+  mainLayout->addLayout(buttonsLayout);
   mainLayout->addWidget(splitter);
 
   setLayout(mainLayout);
@@ -89,23 +92,32 @@ void TableAssistant::clearObjectTable() {
 }
 
 void TableAssistant::forEvents() {
-  _objectsTable->setColumnCount(10);
+  _objectsTable->setColumnCount(11);
   _objectsTable->setColumnHidden(0, true);
   _objectsTable->setColumnHidden(1, true);
 
   // Configure column settings
   _objectsTable->setHorizontalHeaderLabels(QStringList() << "uuid"
                                                          << "filter_names"
-                                                         << ""
                                                          << "Type"
                                                          << "Component Number"
                                                          << "x"
                                                          << "y"
                                                          << "z"
                                                          << "Date"
-                                                         << "Time");
+                                                         << "Time"
+                                                         << ""
+                                                         << "");
 
   _objectsTable->resizeColumnsToContents();
+
+  auto horizontalHeaderObjectTable = _objectsTable->horizontalHeader();
+  horizontalHeaderObjectTable->setSectionResizeMode(
+      10, QHeaderView::Fixed); // fixed remove-button section
+  horizontalHeaderObjectTable->setSectionResizeMode(
+      9, QHeaderView::Stretch); // stretching pred-last section
+  horizontalHeaderObjectTable->setDefaultAlignment(Qt::AlignLeft);
+  _objectsTable->verticalHeader()->setVisible(false);
 }
 
 void TableAssistant::insertRowInFilterTable(const QString &field) {
@@ -171,19 +183,19 @@ void TableAssistant::applyFilterFor(int column, const QString &pattern,
                                     const QString &filterName) {
   if constexpr (std::is_same_v<SeismEvent, mode_t>) {
     switch (column) {
+    case 2:
     case 3:
-    case 4:
       applyFilter<int>(column, pattern, filterName);
       break;
+    case 4:
     case 5:
     case 6:
-    case 7:
       applyFilter<float>(column, pattern, filterName);
       break;
-    case 8:
+    case 7:
       applyFilter<QDate>(column, pattern, filterName);
       break;
-    case 9:
+    case 8:
       applyFilter<QTime>(column, pattern, filterName);
       break;
     }
@@ -232,49 +244,47 @@ void TableAssistant::add<SeismEvent>(const std::unique_ptr<SeismEvent> &event) {
 
   _objectsTable->setItem(row, 1, new QTableWidgetItem());
 
-  QWidget *buttonWidget = new QWidget();
-  QHBoxLayout *buttonLayout = new QHBoxLayout(buttonWidget);
-  buttonLayout->setAlignment(Qt::AlignCenter);
-  QPushButton *removeButton = new QPushButton("Remove");
-  buttonLayout->addWidget(removeButton);
-  buttonLayout->setContentsMargins(0, 0, 0, 0);
-  _objectsTable->setCellWidget(row, 2, buttonWidget);
-  connect(removeButton, &QPushButton::clicked,
-          [this, uuid]() { emit removeClicked(uuid); });
+  _objectsTable->setItem(
+      row, 2, new QTableWidgetItem(QString::number(event->getType())));
 
   _objectsTable->setItem(
-      row, 3, new QTableWidgetItem(QString::number(event->getType())));
-
-  _objectsTable->setItem(
-      row, 4,
+      row, 3,
       new QTableWidgetItem(QString::number(event->getComponentNumber())));
 
   if (event->isProcessed()) {
     auto &location = event->getLocation();
-    _objectsTable->setItem(row, 5,
+    _objectsTable->setItem(row, 4,
                            new QTableWidgetItem(QString::number(
                                static_cast<double>(std::get<0>(location)))));
-    _objectsTable->setItem(row, 6,
+    _objectsTable->setItem(row, 5,
                            new QTableWidgetItem(QString::number(
                                static_cast<double>(std::get<1>(location)))));
-    _objectsTable->setItem(row, 7,
+    _objectsTable->setItem(row, 6,
                            new QTableWidgetItem(QString::number(
                                static_cast<double>(std::get<2>(location)))));
   }
 
   _objectsTable->setItem(
-      row, 8,
+      row, 7,
       new QTableWidgetItem(event->getDateTime().date().toString("dd.MM.yy")));
 
   _objectsTable->setItem(
-      row, 9,
+      row, 8,
       new QTableWidgetItem(event->getDateTime().time().toString("hh:mm")));
+
+  QPushButton *removeButton = new QPushButton();
+  QIcon icon(":/remove_button.png");
+  removeButton->setStyleSheet("background-color:white; border-style: outset");
+  removeButton->setIcon(icon);
+  _objectsTable->setCellWidget(row, 10, removeButton);
+  connect(removeButton, &QPushButton::clicked,
+          [this, uuid]() { emit removeClicked(uuid); });
 
   _objectsTable->resizeColumnsToContents();
   _objectsTable->setSortingEnabled(true);
 }
 
-// NOTE: сохвать ли новые Item`ы или перезаписывать из значения?
+// NOTE: созвать ли новые Item`ы или перезаписывать из значения?
 template <>
 void TableAssistant::update<SeismEvent>(
     const std::unique_ptr<SeismEvent> &event) {
@@ -285,35 +295,35 @@ void TableAssistant::update<SeismEvent>(
     if (uuidEvent ==
         _objectsTable->item(row, 0)->data(Qt::DisplayRole).toUuid()) {
       _objectsTable->setItem(
-          row, 3, new QTableWidgetItem(QString::number(event->getType())));
+          row, 2, new QTableWidgetItem(QString::number(event->getType())));
 
       _objectsTable->setItem(
-          row, 4,
+          row, 3,
           new QTableWidgetItem(QString::number(event->getComponentNumber())));
 
       if (event->isProcessed()) {
         auto &location = event->getLocation();
         _objectsTable->setItem(
-            row, 5,
+            row, 4,
             new QTableWidgetItem(
                 QString::number(static_cast<double>(std::get<0>(location)))));
         _objectsTable->setItem(
-            row, 6,
+            row, 5,
             new QTableWidgetItem(
                 QString::number(static_cast<double>(std::get<1>(location)))));
         _objectsTable->setItem(
-            row, 7,
+            row, 6,
             new QTableWidgetItem(
                 QString::number(static_cast<double>(std::get<2>(location)))));
       }
 
       _objectsTable->setItem(
-          row, 8,
+          row, 7,
           new QTableWidgetItem(
               event->getDateTime().date().toString("dd.MM.yy")));
 
       _objectsTable->setItem(
-          row, 9,
+          row, 8,
           new QTableWidgetItem(event->getDateTime().time().toString("hh:mm")));
 
       break;
